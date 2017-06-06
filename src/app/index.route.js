3c9c7d909a2046fb7f4bba55a165ea46e5e22def
e066532 <Jeyenthaaran.Nanjappan@mastercard.com>
2017-06-06 11:07:02 +0530

    Optimize Trending Movies and Upcoming Movies Queries.

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -15,16 +15,46 @@
         controllerAs: 'vm',
         resolve: {
           trendingMovies: function ($http) {
-            return $http.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6101bba1eadfefbac6a1e1549e861665')
+
+            var storedValue = localStorage.getItem('trendingMovies');
+            var lastUpdate = localStorage.getItem('trendingMoviesUpdateTime');
+
+            var duration = lastUpdate ? (lastUpdate - (new Date).getTime()) : 0;
+
+            if(duration > 3600000)
+                storedValue = null;
+
+            var httpRequest = $http.get('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=6101bba1eadfefbac6a1e1549e861665')
               .then(function (res) {
+                localStorage.setItem('trendingMovies', JSON.stringify(res.data.results));
+                localStorage.setItem('trendingMoviesUpdateTime', (new Date).getTime());
                 return res.data.results;
               });
+
+            return storedValue ? JSON.parse(storedValue) : httpRequest;
+
           },
           upcomingMovies: function ($http) {
-            return $http.get('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2017-05-24&api_key=6101bba1eadfefbac6a1e1549e861665')
+
+            var storedValue = localStorage.getItem('upcomingMovies');
+            var lastUpdate = localStorage.getItem('upcomingMoviesUpdateTime');
+
+            var duration = lastUpdate ? (lastUpdate - (new Date).getTime()) : 0;
+
+            if(duration > 3600000)
+              storedValue = null;
+
+            var today = new Date();
+            var queryDate = (today.getFullYear() + 1) + '-' + today.getMonth() + '-' +  today.getDate();
+
+            var httpRequest = $http.get('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=' + queryDate +'&api_key=6101bba1eadfefbac6a1e1549e861665')
               .then(function (res) {
+                localStorage.setItem('upcomingMovies', JSON.stringify(res.data.results));
+                localStorage.setItem('upcomingMoviesUpdateTime', (new Date).getTime());
                 return res.data.results;
               });
+
+            return storedValue ? JSON.parse(storedValue) : httpRequest;
           }
         }
       });
